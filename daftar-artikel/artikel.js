b749eea15c29b0700b4c2af0a6d3362b4b7328f8
meisyareptiana <meisyareptiana5@gmail.com>
2023-05-05 20:21:14 +0700

    create card content

--- daftar-artikel/artikel.js
+++ daftar-artikel/artikel.js
@@ -6,40 +6,39 @@ fetch("artikel.json")
   .then((data) => {
     // Membuat sebuah variabel untuk menampung daftar card
     const daftarCard = document.getElementById("card-artikel");
+    daftarCard.classList.add("row", "gy-4");
 
     // Menggunakan loop untuk membuat card HTML untuk setiap objek dalam file JSON
-    data.slice(0, 3).forEach((item) => {
+    data.forEach((item) => {
+      //.slice(0, 3)
       // Membuat elemen card
       const card = document.createElement("div");
-      card.classList.add("col-lg-4 col-md-6");
-      card.style.width = "18rem";
+      card.classList.add("col-lg-4", "col-md-6");
+
+      const containerCard = document.createElement("div");
+      card.classList.add("card");
 
       // Menambahkan gambar pada card
       const img = document.createElement("img");
       img.classList.add("card-img-top");
       img.src = item.gambar;
       img.alt = "gambar";
-      // card.appendChild(img);
 
       // Menambahkan isi card
       const cardBody = document.createElement("div");
       cardBody.classList.add("card-body");
-      // card.appendChild(cardBody);
 
       const kategori = document.createElement("p");
       kategori.classList.add("card-text", "text-primary");
       kategori.textContent = item.kategori;
-      // cardBody.appendChild(kategori);
 
       const judul = document.createElement("h5");
       judul.classList.add("card-title");
       judul.textContent = item.judul;
-      // cardBody.appendChild(judul);
 
       const isi = document.createElement("p");
       isi.classList.add("card-text", "text-secondary");
       isi.textContent = item.isi;
-      // cardBody.appendChild(isi);
 
       // Menambahkan elemen-elemen ke dalam card
       cardBody.appendChild(kategori);
@@ -49,8 +48,11 @@ fetch("artikel.json")
       card.appendChild(img);
       card.appendChild(cardBody);
 
+      card.appendChild(containerCard);
       // Menambahkan card ke dalam daftar card
       daftarCard.appendChild(card);
+
+      console.log(daftarCard);
     });
   })
   .catch((error) => console.error(error));
