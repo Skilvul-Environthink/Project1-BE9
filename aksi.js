ad1dfda268749aed95540ad411fb33d22e99c42c
meisyareptiana <meisyareptiana5@gmail.com>
2023-05-06 18:47:11 +0700

    update navbar aksi

--- aksi.js
+++ aksi.js
@@ -1,3 +1,20 @@
+let user = localStorage.getItem("user");
+const avatar = document.querySelector(".avatar-wrapper img");
+const dropdownMenu = document.getElementById("dropdown-menu");
+const logoutBtn = document.getElementById("logout-btn");
+user = JSON.parse(user);
+console.log(user);
+avatar.addEventListener("click", function () {
+  dropdownMenu.style.display = "block";
+});
+const menu = document.getElementById("menu-navbar");
+const subMenu = document.createElement("li");
+
+logoutBtn.addEventListener("click", function () {
+  console.log("Logout clicked");
+  window.location.href = "/Project1-BE9/log.html"; // redirect to logout page
+});
+
 // Mengambil data dari file JSON menggunakan fetch API
 // import data from "./aksi.json" assert { type: "json" };
 // console.log(data);
